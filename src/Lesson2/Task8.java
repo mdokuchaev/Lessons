6941f6e307aedfa5e0f9ae694ad01a438f2617ab
MDokuchaev <M.Dokuchaev@x5.ru>
2020-09-12 19:38:36 +0300

    Add Task7

---
+++ src/Lesson2/Task8.java
@@ -0,0 +1,53 @@
+/*
+Дан массив целых чисел: [1,-10,5,6,45,23,-45,-34,0,32,56,-1,2,-2].
+Для данного массива найти и вывести на экран:
+- максимальное значение
+- сумму положительных элементов
+- сумму четных отрицательных элементов
+- количество положительных элементов
+- среднее арифметическое отрицательных элементов
+*/
+
+package Lesson2;
+
+import java.util.Arrays;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+
+        int[] massInt = {1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2};
+        System.out.println("Массив целых чисел: " + Arrays.toString(massInt));
+        int maxElement = massInt[0];
+        int sumPositiveElements = 0, sumEvenNegativEelements = 0, sumNegativEelements = 0;
+        int countPositiveElements = 0, countNegativElements = 0;
+        double averageNegativElements = 0;
+
+        for (int i = 0; i < massInt.length; i++) {
+            if (massInt[i] > maxElement) {
+                maxElement = massInt[i]; // максимальное значение
+            }
+            if (massInt[i] >= 0) {
+                sumPositiveElements += massInt[i]; // сумма положительных элементов
+                countPositiveElements++; //количество положительных элементов
+            } else {
+                if (massInt[i] % 2 == 0) {
+                    sumEvenNegativEelements += massInt[i]; // сумма четных отрицательных элементов
+                }
+                sumNegativEelements += massInt[i]; // сумма отрицательных элементов
+                countNegativElements++; // количество отрицательных элементов
+            }
+        }
+
+        if (countNegativElements > 0) {
+            averageNegativElements = sumNegativEelements * 1.0 / countNegativElements;
+        }
+
+        System.out.println("Максимальное значение в массиве: " + maxElement);
+        System.out.println("Сумма положительных элементов в массиве: " + sumPositiveElements);
+        System.out.println("Cумма четных отрицательных элементов в массиве: " + sumEvenNegativEelements);
+        System.out.println("Количество положительных элементов в массиве: " + countPositiveElements);
+        System.out.println("Среднее арифметическое отрицательных элементов в массиве: " + averageNegativElements);
+    }
+
+}
---
+++ src/Lesson2/Task9.java
@@ -0,0 +1,29 @@
+/*
+Дан массив целых чисел: [15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52].
+Переставить элементы массива в обратном порядке.
+Вывести результат в консоль.
+*/
+
+package Lesson2;
+
+import java.util.Arrays;
+
+public class Task9 {
+
+    public static void main(String[] args) {
+
+        int[] massInt = {15, 10, 51, -6, -5, 3, -10, -34, 0, 32, 56, -12, 24, -52};
+        System.out.println("Массив целых чисел: " + Arrays.toString(massInt));
+
+        int tempInt;
+
+        for (int i = 0; i < massInt.length / 2; i++) {
+
+            tempInt = massInt[i];
+            massInt[i] = massInt[massInt.length - 1 - i];
+            massInt[massInt.length - 1 - i] = tempInt;
+
+        }
+        System.out.println("Массив целых чисел в обратном порядке: " + Arrays.toString(massInt));
+    }
+}
